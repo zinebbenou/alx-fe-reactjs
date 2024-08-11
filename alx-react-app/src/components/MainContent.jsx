cdbc1b3e5e2152ccfdd048f5506f87a28a1f9a33
zinebbenou <zineb_ben000@hotmail.fr>
2024-08-11 20:47:09 +0800

    Added Header, MainContent, and Footer components and updated App.jsx

---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+ // src/components/MainContent.jsx
+function MainContent() {
+  return (
+    <main>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
+
