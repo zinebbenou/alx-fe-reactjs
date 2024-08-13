2fcde23898d05f351f64e4b11a08b84a3cb17f19
zinebbenou <zineb_ben000@hotmail.fr>
2024-08-13 23:25:28 +0800

    Add Navbar component and update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
+import Navbar from './components/Navbar';  // Import Navbar
 
 function App() {
   return (
     <Router>
+      <Navbar />  {/* Include Navbar */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
