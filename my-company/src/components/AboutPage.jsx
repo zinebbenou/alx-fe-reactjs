fb9490d1fa928206a24c0bfebce7e03719b0c4ad
zinebbenou <zineb_ben000@hotmail.fr>
2024-08-13 23:14:14 +0800

    Added content to HomePage and AboutPage components

---
+++ my-company/src/components/AboutPage.jsx
@@ -0,0 +1,13 @@
+// src/components/AboutPage.jsx
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Learn more about us on this page.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
