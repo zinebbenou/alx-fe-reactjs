b134a91b48a6716ebc74b8bb67e7d1118c879994
zinebbenou <zineb_ben000@hotmail.fr>
2024-08-13 23:27:14 +0800

    Add Contact component

---
+++ my-company/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+// src/components/Contact.jsx
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>This is the contact page.</p>
+    </div>
+  );
+}
+
+export default Contact;
