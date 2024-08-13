3f51ec4823bfd7bc90f336be8209725c2af189aa
zinebbenou <zineb_ben000@hotmail.fr>
2024-08-13 23:21:55 +0800

    Add App.jsx for routing implementation

---
+++ my-company/src/App.jsx
@@ -0,0 +1,18 @@
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
