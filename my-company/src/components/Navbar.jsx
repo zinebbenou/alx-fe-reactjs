6632ef81f254c707fba44057b0f5a028e3c321e9
zinebbenou <zineb_ben000@hotmail.fr>
2024-08-13 23:30:50 +0800

    Add inline styling to Navbar component

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,12 +3,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  // Define inline styles
+  const navbarStyle = {
+    backgroundColor: '#282c34', // Example background color
+    display: 'flex',           // Use flexbox layout
+    justifyContent: 'space-between', // Space items evenly
+    padding: '1rem',           // Add padding around the navbar
+    color: 'white'             // Text color
+  };
+
+  const linkStyle = {
+    color: 'white',            // Link text color
+    textDecoration: 'none'     // Remove underline from links
+  };
+
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
+    <nav style={navbarStyle}>
+      <div>
+        <Link to="/" style={linkStyle}>Home</Link>
+        <Link to="/about" style={linkStyle}>About</Link>
+        <Link to="/contact" style={linkStyle}>Contact</Link>
+      </div>
     </nav>
   );
 }
