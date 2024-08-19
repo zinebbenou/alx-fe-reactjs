7b6bffd6efe933446216da5434adc866dc1e9b51
zinebbenou <zineb_ben000@hotmail.fr>
2024-08-19 20:14:09 +0800

    Add initial recipe sharing app

---
+++ src/components/RecipeList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useRecipeStore } from '../recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore(state => state.recipes);
+
+  return (
+    <div>
+      {recipes.map(recipe => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
