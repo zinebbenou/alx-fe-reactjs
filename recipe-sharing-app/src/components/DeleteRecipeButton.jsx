954660bbfacafaa877bdda12bdff0f7713b7dbae
zinebbenou <zineb_ben000@hotmail.fr>
2024-08-20 00:44:45 +0800

    Add: Implement DeleteRecipeButton and Zustand store

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 import { useRecipeStore } from '../recipeStore';
 
 const DeleteRecipeButton = ({ recipeId }) => {
-  const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
 
   const handleDelete = () => {
-    if (window.confirm('Are you sure you want to delete this recipe?')) {
+    if (window.confirm("Are you sure you want to delete this recipe?")) {
       deleteRecipe(recipeId);
     }
   };
@@ -20,4 +20,3 @@ const DeleteRecipeButton = ({ recipeId }) => {
 };
 
 export default DeleteRecipeButton;
-
