4eccffab32e4f098753eceb1b0bd969ef98ccd0e
zinebbenou <zineb_ben000@hotmail.fr>
2024-08-29 17:04:36 +0800

    Add Formik form with validation to formikForm.js

--- form-handling-react/src/components/FormikForm.js
+++ form-handling-react/src/components/FormikForm.js
@@ -2,44 +2,50 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
+// Validation schema using Yup
+const validationSchema = Yup.object({
+  username: Yup.string().required('Username is required'),
+  email: Yup.string().email('Invalid email address').required('Email is required'),
+  password: Yup.string().required('Password is required'),
+});
+
 const FormikForm = () => {
-  const initialValues = {
-    username: '',
-    email: '',
-    password: ''
-  };
-
-  const validationSchema = Yup.object({
-    username: Yup.string().required('Username is required'),
-    email: Yup.string().email('Invalid email format').required('Email is required'),
-    password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required')
-  });
-
-  const handleSubmit = (values) => {
-    console.log('Form submitted', values);
-  };
-
   return (
     <Formik
-      initialValues={initialValues}
+      initialValues={{ username: '', email: '', password: '' }}
       validationSchema={validationSchema}
-      onSubmit={handleSubmit}
+      onSubmit={(values) => {
+        // Simulate form submission
+        console.log('Form submitted:', values);
+      }}
     >
       <Form>
         <div>
-          <label>Username:</label>
-          <Field type="text" name="username" />
-          <ErrorMessage name="username" component="div" />
+          <label htmlFor="username">Username:</label>
+          <Field
+            type="text"
+            id="username"
+            name="username"
+          />
+          <ErrorMessage name="username" component="div" style={{ color: 'red' }} />
         </div>
         <div>
-          <label>Email:</label>
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="div" />
+          <label htmlFor="email">Email:</label>
+          <Field
+            type="email"
+            id="email"
+            name="email"
+          />
+          <ErrorMessage name="email" component="div" style={{ color: 'red' }} />
         </div>
         <div>
-          <label>Password:</label>
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" component="div" />
+          <label htmlFor="password">Password:</label>
+          <Field
+            type="password"
+            id="password"
+            name="password"
+          />
+          <ErrorMessage name="password" component="div" style={{ color: 'red' }} />
         </div>
         <button type="submit">Register</button>
       </Form>
