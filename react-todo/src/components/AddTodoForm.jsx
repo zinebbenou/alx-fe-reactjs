a8a5c05438e56ed93b0d1a8cbc1d1aae3dc603c0
zinebbenou <zineb_ben000@hotmail.fr>
2024-09-01 08:47:04 +0800

    Initial commit with TodoList component and tests

---
+++ react-todo/src/components/AddTodoForm.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const AddTodoForm = ({ addTodo }) => {
+  const [text, setText] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text) {
+      addTodo(text);
+      setText('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Add a new todo"
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default AddTodoForm;
---
+++ react-todo/src/components/TodoList.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import AddTodoForm from './AddTodoForm';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React', completed: false },
+    { id: 2, text: 'Learn Jest', completed: false },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodo = { id: Date.now(), text, completed: false };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(todos.map(todo =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    ));
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <AddTodoForm addTodo={addTodo} />
+      <ul>
+        {todos.map(todo => (
+          <li
+            key={todo.id}
+            onClick={() => toggleTodo(todo.id)}
+            style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+          >
+            {todo.text}
+            <button onClick={(e) => { e.stopPropagation(); deleteTodo(todo.id); }}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
